95d276708b22d2c4463d6cf2226fe9dcf1438a08
unknown <wisnukm41@gmail.com>
2020-01-30 12:46:15 +0700

    update DB

--- tiketBus.sql
+++ tiketBus.sql
@@ -28,6 +28,27 @@ CREATE TABLE `t_bus` (
   PRIMARY KEY (`kode_bus`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+/*Data for the table `t_bus` */
+
+insert  into `t_bus`(`kode_bus`,`jmlh_seat`,`jam_berangkat`,`aktif`) values 
+('Batal',0,'00:00:00',0),
+('BJ-01',34,'15:00:00',1),
+('BJ-02',34,'15:00:00',1),
+('DS-01',34,'08:00:00',1),
+('DS-02',34,'08:00:00',1),
+('DU-01',34,'14:00:00',1),
+('DU-02',34,'14:00:00',1),
+('JG-01',34,'18:00:00',1),
+('JG-02',34,'18:00:00',1),
+('LM-01',34,'17:00:00',1),
+('LM-02',34,'17:00:00',1),
+('LP-01',34,'19:00:00',1),
+('LP-02',34,'19:00:00',1),
+('MG-01',34,'13:00:00',1),
+('MG-02',34,'13:00:00',1),
+('SO-01',34,'17:30:00',1),
+('SO-02',34,'17:30:00',1);
+
 /*Table structure for table `t_perjalanan` */
 
 DROP TABLE IF EXISTS `t_perjalanan`;
@@ -44,6 +65,26 @@ CREATE TABLE `t_perjalanan` (
   CONSTRAINT `t_perjalanan_ibfk_1` FOREIGN KEY (`kode_bus`) REFERENCES `t_bus` (`kode_bus`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+/*Data for the table `t_perjalanan` */
+
+insert  into `t_perjalanan`(`id_perjalanan`,`tujuan`,`harga`,`makan`,`kode_bus`,`aktif`) values 
+('T-001','Bojonegoro',220000,1,'BJ-01',1),
+('T-002','Bojonegoro',220000,1,'BJ-02',1),
+('T-003','Denpasar L SLTN',480000,2,'DS-01',1),
+('T-004','Denpasar L SLTN',480000,2,'DS-02',1),
+('T-005','Denpasar L UTR',500000,2,'DU-01',1),
+('T-006','Denpasar L UTR',500000,2,'DU-02',1),
+('T-007','Malang',300000,2,'MG-01',1),
+('T-008','Malang',300000,2,'MG-02',1),
+('T-009','Lasem',180000,1,'LM-01',1),
+('T-010','Lasem',180000,1,'LM-02',1),
+('T-011','Solo',180000,1,'SO-01',1),
+('T-012','Solo',180000,1,'SO-02',1),
+('T-013','Jogjakarta',160000,1,'JG-01',1),
+('T-014','Jogjakarta',160000,1,'JG-02',1),
+('T-015','Lampung',250000,2,'LP-01',1),
+('T-016','Lampung',250000,2,'LP-02',1);
+
 /*Table structure for table `t_tiket` */
 
 DROP TABLE IF EXISTS `t_tiket`;
@@ -59,8 +100,15 @@ CREATE TABLE `t_tiket` (
   `aktif` tinyint(1) DEFAULT 1,
   PRIMARY KEY (`id_tiket`),
   KEY `t_tiket_ibfk_1` (`kode_bus`),
-  CONSTRAINT `t_tiket_ibfk_1` FOREIGN KEY (`kode_bus`) REFERENCES `t_bus` (`kode_bus`) ON DELETE NO ACTION ON UPDATE CASCADE
+  CONSTRAINT `t_tiket_ibfk_1` FOREIGN KEY (`kode_bus`) REFERENCES `t_bus` (`kode_bus`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `t_tiket` */
+
+insert  into `t_tiket`(`id_tiket`,`ktp`,`nama_pemesan`,`tujuan`,`tanggal_pemesanan`,`tanggal_berangkat`,`kode_bus`,`aktif`) values 
+('B-6013615','1672637871676253','SUSAN Y','Lampung','2020-01-30','2020-01-30','LP-01',1),
+('B-7672477','1717262182811881','YAYAT SUPRIYATNA','Bojonegoro','2020-01-30','2020-01-31','BJ-02',1),
+('B-8142180','1234445456667687','YURA','Lasem','2020-01-30','2020-01-30','LM-02',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
